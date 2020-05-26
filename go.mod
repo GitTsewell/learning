11cfe6cafc0cd9cdfe3f000856f8a1e7770eac95
GitTsewell <18008072409@163.com>
2020-05-26 12:55:31 +0800

    kafka

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module learning
 go 1.13
 
 require (
+	github.com/Shopify/sarama v1.26.4
 	github.com/golang/protobuf v1.3.3
 	github.com/nsqio/go-nsq v1.0.8
 	google.golang.org/grpc v1.29.1
---
+++ kafka/kafka_receive.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/Shopify/sarama"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func main() {
+	consumer, err := sarama.NewConsumer([]string{"192.168.3.6:9092"}, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	partitionList, err := consumer.Partitions("web_log")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for partition := range partitionList {
+		pc, err := consumer.ConsumePartition("web_log", int32(partition), sarama.OffsetNewest)
+		if err != nil {
+			panic(err)
+		}
+
+		defer pc.AsyncClose()
+
+		wg.Add(1)
+
+		go func(sarama.PartitionConsumer) {
+			defer wg.Done()
+			for msg := range pc.Messages() {
+				fmt.Printf("Partition:%d, Offset:%d, Key:%s, Value:%s\n", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+			}
+		}(pc)
+		wg.Wait()
+		consumer.Close()
+	}
+}
